e9638b3fcbd94deca26fe6632c73f8d908d66640
mhnd-lab <59766051+mhnd-lab@users.noreply.github.com>
2020-11-01 17:27:40 +0300

    Add files via upload

--- EEWS/deprem.py
+++ EEWS/deprem.py
@@ -9,15 +9,18 @@ ctx.check_hostname = False
 ctx.verify_mode = ssl.CERT_NONE
 
 def watchEQ():
+    counter = 0
     try:
         print("Monitoring started ....\n")
-
         while True:
+            counter = counter + 1
+            print("--- !!! ",counter," !!! ---")
             url = ('http://www.koeri.boun.edu.tr/scripts/lst9.asp')
             try:
                 html = urllib.request.urlopen(url, context=ctx).read()
             except:
                 print("!!! NETWORK ERROR !!!")
+                watchEQ()
 
             soup = BeautifulSoup(html, 'html.parser')
             #retrive raw data
@@ -55,7 +58,7 @@ def watchEQ():
             print("Region :", results["region"])
             print("\n")
 
-            time.sleep(60)
+            time.sleep(1)
     except:
 
         print("!!! RESTARTING !!!")
