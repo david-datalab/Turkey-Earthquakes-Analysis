a0cc9c1274e76d98429fbe6b7d7548f39e9f9758
mhnd-lab <daoudmhnd@gmail.com>
2021-06-09 22:13:21 +0300

    new file structure and new data base getting ready for data analysis

---
+++ main.py
@@ -0,0 +1,110 @@
+import requests
+from bs4 import BeautifulSoup
+import re
+import sqlite3
+import time as T
+
+print('Started')
+
+try:
+    #Database creation
+    conn = sqlite3.connect('Database.sqlite')
+    #Database Handler
+    cur = conn.cursor()
+    #Database table
+    cur.executescript('''
+    CREATE TABLE IF NOT EXISTS EarthQuake (
+    id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
+    date TEXT,
+    time TEXT UNIQUE,
+    latitiude TEXT,
+    longitude TEXT,
+    depth TEXT,
+    magnitude TEXT,
+    region TEXT
+    )
+    ''')
+
+except Exception as E1:
+    print('E1: Database creation error')
+    print(E1)
+
+#the program will loop for ever to keep refreshing the data source every 15 seconds
+
+while True:
+    try:
+        #preparing the data source
+        url = ('http://www.koeri.boun.edu.tr/scripts/lst9.asp')
+        html = requests.get(url).text
+        soup = BeautifulSoup(html, "html.parser")
+        tags = soup('pre')
+        lst = list()
+
+    except Exception as E2:
+        print('E2: Connection Error')
+        print(E2)
+
+    try:
+        #looping through the strings inside the pre tag and add the results to a list
+        for tag in tags:
+            fh = tag
+
+            for line in fh:
+                data = line
+                ln = re.findall("(\S.*)", data)
+
+                for s in ln:
+                    if s not in lst:#UPDATE: to add the string value in a single list
+                        lst.append(s)
+
+    except Exception as E3:
+        print('E3: Error while scraping data from source')
+        print(E3)
+
+    #cleaning the retrived data and prepare the requried lines for analysis
+    lastEQ = lst[6].split()
+    date = lastEQ[0]
+    time = lastEQ[1]
+    latitiude = lastEQ[2]
+    longitude = lastEQ[3]
+    depth = lastEQ[4]
+    magnitude = lastEQ[6]
+    region = " ".join(lastEQ[8:-1])
+    lstResults = [date,time,latitiude,longitude,depth,magnitude,region]
+    #now we have a list of the data needed for the analysis
+    #we will add the data to sqlite data base to proceed with the analysis
+    #to fill the records in the database
+    try:
+        #first we start to write the data to the database
+        cur.execute('''INSERT OR IGNORE INTO earthQuake (date, time, latitiude, longitude, depth, magnitude, region)
+        VALUES (?,?,?,?,?,?,?)''',(date, time, latitiude, longitude, depth, magnitude, region))
+    
+    except Exception as E4:
+        print('E4: Database writing error')
+        print(E4)
+
+    try:
+        #to check the results
+        #first we divide the data as strings 
+        formated = "{},{},{},{},{},{},{}\n".format(lstResults[0], lstResults[1], lstResults[2], lstResults[3], lstResults[4] ,lstResults[5], lstResults[6])
+    
+        #we look for the longitude and compare it with recorded results
+        cur.execute('SELECT longitude FROM earthQuake ORDER BY id DESC')
+        lon = cur.fetchone()
+        slon = str(lon[0])
+
+        #we look for the latitude and compare it with recorded results
+        cur.execute('SELECT latitiude FROM earthQuake ORDER BY id DESC')
+        lat = cur.fetchone()
+        slat = str(lat[0])
+        #we compare the values and print the last record to avoid printing new lines for each execution
+
+        if slon != longitude and slat != latitiude:
+            print(formated)
+
+    except Exception as E5:
+        print('E5: Data checking error')
+        print(E5)
+
+    conn.commit()
+    T.sleep(15)
---
+++ old.py
@@ -0,0 +1,124 @@
+"""
+WebLinez, hereby disclaims all copyright interest in the program “LockQuake” (which monitor and analyze earthquakes in Turkey) written by Muhannad Daoud.
+
+signature of Muhannad Daoud 31 march 2021
+Muhannad Daoud WebLinez Owner
+
+Copyright 2020, 2021 Muhannad Daoud
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>. 
+"""
+
+
+import requests
+from bs4 import BeautifulSoup
+import ssl
+import re
+import time
+import sqlite3
+import sys
+
+# Ignore SSL certificate errors
+ctx = ssl.create_default_context()
+ctx.check_hostname = False
+ctx.verify_mode = ssl.CERT_NONE
+
+#database creation
+conn = sqlite3.connect('Database.sqlite')
+cur = conn.cursor()
+cur.executescript('''
+CREATE TABLE IF NOT EXISTS earthQuake (
+    id     INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
+    date   TEXT,
+    time    TEXT UNIQUE,
+    latitiude   TEXT,
+    longitude  TEXT,
+    depth   TEXT,
+    magnitude   TEXT,
+    region  TEXT
+)
+''')
+
+print('started')
+
+while True:
+    # Check to see if we are already in progress...
+    cur.execute('SELECT id,time FROM earthQuake WHERE latitiude is NULL and longitude is NULL ORDER BY RANDOM() LIMIT 1')
+    row = cur.fetchone()
+    if row is not None :
+        print("Restarting existing crawl.  Remove old database to start a fresh crawl.")
+    else:
+        lst=list()
+        url = ('http://www.koeri.boun.edu.tr/scripts/lst9.asp')
+        try:
+            html = requests.get(url).text 
+        except:
+            print("!!! NETWORK ERROR !!!")
+
+        soup = BeautifulSoup(html, "html.parser")
+        #retrive raw data
+        tags = soup('pre')
+        # Look at the parts of a tag
+        for tag in tags:
+            fh = tag
+
+            for line in fh:#to extract valuse and put them in a list
+                data = line
+                ln = re.findall("(\S.*)", data)
+
+                for s in ln: #to loop through the strings found in ln
+                    if s not in lst:#UPDATE: the headache was here XD #to add the string value in a single list
+                        #print(s)
+                        lst.append(s)
+
+        lastEQ = lst[6].split()
+        mag = lastEQ[5] + " " + lastEQ[6] + " " + lastEQ[7]
+        reg = " ".join(lastEQ[8:-1])
+        date = lastEQ[0]
+        tim = lastEQ[1]
+        latitiude = lastEQ[2]
+        longitude = lastEQ[3]
+        #print(longitude)
+        depth = lastEQ[4]
+        magnitude = mag
+        region = reg
+        lstResults = [date,tim,latitiude,longitude,depth,magnitude,region]
+        #print(lstResults)
+        try:
+            #read last value from data base and compare it to last reading value
+            cur.execute('SELECT longitude FROM earthQuake ORDER BY id DESC')
+            lon = cur.fetchone()
+            print(lon)
+            slon = str(lon[0])
+            try:
+                cur.execute('SELECT latitiude FROM earthQuake ORDER BY id DESC')
+                lat = cur.fetchone()
+                slat = str(lat[0])
+            except Exception as e:
+                print(e)
+
+            if slon != longitude and slat != latitiude:
+                #fh = open('records.csv',"a+")
+                formated = "{},{},{},{},{},{},{}\n".format(lstResults[0], lstResults[1], lstResults[2], lstResults[3], lstResults[4], lstResults[5], lstResults[6])
+                #print(formated)
+                #fh.write(formated)
+                #fh.close()
+                cur.execute('''INSERT OR IGNORE INTO earthQuake (date, time, latitiude, longitude, depth, magnitude, region)
+                VALUES (?,?,?,?,?,?,?)''', (date,tim,latitiude,longitude,depth,magnitude,region))
+        except Exception as e:
+            z = e
+            print(z)
+
+        conn.commit()
+        time.sleep(3)
